7d04fbc2ccd5cf1dbee9eb51ab5843ed23366ae5
park-yina <kln99988@naver.com>
2024-02-09 10:18:45 +0900

    fix:아코디온의 클릭범위 수정

--- ch_maple/static/accordion.js
+++ ch_maple/static/accordion.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const title1=document.querySelector(".title1");
+    const title2=document.querySelector(".title2");
+    const title3=document.querySelector(".title3");
+
     const arrow1 = document.querySelector("#arrow1");
 const arrow2 = document.querySelector('#arrow2');
 const arrow3 = document.querySelector('#arrow3');
 const desc1 = document.querySelector('.title1 + .desc');
 const desc2 = document.querySelector('.title2 + .desc');
 const desc3 = document.querySelector('.title3 + .desc');
-    arrow1.addEventListener('click', function() {
+    title1.addEventListener('click', function() {
     if (desc1.style.display === 'none') {
         desc1.style.display = 'block';
         arrow1.classList.remove('fa-angles-up');
@@ -16,7 +20,7 @@ const desc3 = document.querySelector('.title3 + .desc');
         arrow1.classList.add('fa-angles-up');
     }
 });
-    arrow2.addEventListener('click', function() {
+    title2.addEventListener('click', function() {
         if (desc2.style.display === 'none') {
         desc2.style.display = 'block';
         arrow2.classList.remove('fa-angles-up');
@@ -28,7 +32,7 @@ const desc3 = document.querySelector('.title3 + .desc');
         arrow2.classList.add('fa-angles-up');
         }
     });
-    arrow3.addEventListener('click', function() {
+    title3.addEventListener('click', function() {
         if (desc3.style.display === 'none') {
         desc3.style.display = 'block';
         arrow3.classList.remove('fa-angles-up');
